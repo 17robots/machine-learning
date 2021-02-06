fb99bb7eefcb35107f385973bc53d690738f7cbb
17robots <mdray@ameritech.net>
2021-02-06 15:46:33 -0500

    Add Proper 3d Graph

--- flowerKNN.py
+++ flowerKNN.py
@@ -15,7 +15,7 @@ lookup_flower_name = dict(
 
 print("Model: Iris Flower Predictions")
 print("Number of Attributes: 4, petal length, petal width, sepal length and sepal width")
-print(lookup_flower_name)
+# print(lookup_flower_name)
 
 X = flowers[['petal_length', 'petal_width', 'sepal_length', 'sepal_width']]
 Y = flowers['species']
@@ -27,22 +27,24 @@ scatter = scatter_matrix(X_train, c=Y_train, marker='o', s=40, hist_kwds={
                          'bins': 15}, figsize=(9, 9), cmap=cmap)
 
 fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.scatter(X_train['petal_width'], X_train['petal_length'],
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X_train['sepal_length'], X_train['sepal_width'], X_train['petal_length'],
            c=Y_train, marker='o', s=100)
-ax.set_xlabel('petal width')
-ax.set_ylabel('petal length')
+ax.set_xlabel('sepal width')
+ax.set_ylabel('sepal length')
+ax.set_zlabel('petal length')
 plt.show()
 
-knn = KNeighborsClassifier(n_neighbors=5, weight='uniform')
+knn = KNeighborsClassifier(n_neighbors=5, weights='uniform')
 
 knn.fit(X_train, Y_train)
 
 # calculate the validity of the model for future predictions
 knn.score(X_test, Y_test)
 
-flower_prediction = knn.predict([7, 2.9, 5.7, 1.8])
-print(lookup_flower_name(flower_prediction[0]))
+# lets make a prediction for a flower
+flower_prediction = knn.predict([[7, 2.9, 5.7, 1.8]])
+print(lookup_flower_name[flower_prediction[0]])
 
 # calculate the sensitivity of the k-NN classification accuracy to the choice of the k parameter
 k_range = range(1, 20)
@@ -68,7 +70,7 @@ for s in t:
         X_train, X_test, Y_train, Y_test = train_test_split(
             X, Y, test_size=1-s)
         knn.fit(X_train, Y_train)
-        scores.append(knn.scores(X_test, Y_test))
+        scores.append(knn.score(X_test, Y_test))
     plt.plot(s, np.mean(scores), 'bo')
 
 plt.xlabel('Training Test Proportion (%)')
